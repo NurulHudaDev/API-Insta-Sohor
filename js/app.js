3c94d1b51c65792e3432146530ad01e4ebb5680d
NurulHudaDev <nurulhuda22dev@gmail.com>
2022-03-11 09:33:33 +0300

    insta-shohor-NurulHudaDev" git add .
    git commit -m insta-shohor-NurulHudaDev
    git commit -m insta-shohor-NurulHudaDev"

--- js/app.js
+++ js/app.js
@@ -18,6 +18,7 @@ const isLiked = (id) => {
 const addToLiked = (id) => {
     likedPostsId.push(id); 
     showPosts(posts);
+    reportPost();
 };
 
 const reportPost = (id) => {
@@ -143,6 +144,7 @@ const showPosts = (posts) => {
 };
 
 const displayLikedPosts = () => {
+  document.getElementById( "liked" ).innerHTML = '';
     const likedPosts = getLikedPosts();
     likedPosts.forEach((post) => {
         const div = createPost(post);
@@ -151,8 +153,9 @@ const displayLikedPosts = () => {
 };
 
 const displayReportedPosts = () => {
+  document.getElementById( "reported" ).innerHTML = '';
     const reportedPosts = getReportedPosts();
-    posts.forEach((post) => {
+    reportedPosts.forEach((post) => {
         const div = createPost(post);
         document.getElementById( "reported" ).appendChild(div);
     });
